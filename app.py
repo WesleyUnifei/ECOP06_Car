395698c94d31891a00e3c70bb6f5810726e034ac
WesleyUnifei <146854141+WesleyUnifei@users.noreply.github.com>
2023-10-23 12:23:07 +0000

    novo

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ st.set_page_config(
 
 st.title('Página Demo ECOP06')
 
-esportes = pd.read_csv('https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv')
+esportes = pd.read_csv('https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv', encoding = 'latin1')
 
 st.dataframe(esportes)
